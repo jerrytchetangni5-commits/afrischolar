df0681a2465704935f56699e5625221ade2e4622
jerrytchetangni5-commits <jerrytchetangni5@gmail.com>
2026-07-09 18:30:00 +0100

    update

--- backend/resources/views/vendor/mail/html/header.blade.php
+++ backend/resources/views/vendor/mail/html/header.blade.php
@@ -1,8 +1,16 @@
-@props(['url'])
+<!-- @props(['url'])
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
     <img src="{{ asset('images/next.png') }}" alt="Next" style="max-height: 50px; display: inline-block;">
+</a>
+</td>
+</tr> -->
+ @props(['url'])
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+    <img src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path('images/next.png'))) }}" alt="Next" style="max-height: 50px;">
 </a>
 </td>
 </tr>
